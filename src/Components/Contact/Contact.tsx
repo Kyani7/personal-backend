8fed56240b639025554bc38354f619e0b35b3cc1
NISCHAL RAI <nischal.c.r1313@gmail.com>
2026-07-13 14:13:54 +0545

    Routing in  contact us

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,9 +1,11 @@
 import ContactHero from "./ContactHero";
 import ContactForm from "./ContactForm";
 import FAQSection from "../Home/faqsSection/faqsSection";
+import usePageTitle from "../../Hook/titleTab";
 
 const ContactMain = () => {
   return (
+    usePageTitle("Our Service | Study Abroad Services");
     <>
       <ContactHero />
       <ContactForm />
